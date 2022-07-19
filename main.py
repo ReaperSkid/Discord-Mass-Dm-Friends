49577120d1672a4772f07c3e9e95839b39d2646c
Reaper <109579027+ReaperSkid@users.noreply.github.com>
2022-07-19 18:56:51 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+from email import message
+import discord
+client = discord.Client()
+import colorama
+from colorama import Fore, Back, Style
+colorama.init()
+
+Message = input("What would you like to Mass DM? [==>]: ")
+
+@client.event
+async def on_connect():
+    for user in client.user.friends:
+        try:
+            await user.send(message)
+            Fore.GREEN
+            print(f"message: {user.name}")
+        except:
+            Fore.RED
+            print(f"couldnt message: {user.name}")
+
+client.run("YOUR DISCORD TOKEN HERE", bot=False)
